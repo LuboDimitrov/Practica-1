7a2e92f143b6cbd0a0d528fc01c35341ca4e2dd5
Lyubomir Dimitrov <78859431+LuboDimitrov@users.noreply.github.com>
2022-10-11 17:41:12 +0200

    Update Server1.c

--- Server1.c
+++ Server1.c
@@ -10,6 +10,7 @@
 #include <sys/types.h>
 #include <signal.h>
 
+//Constants
 #define MAX_CLIENTS 5
 #define BUFFER_SIZE 2048
 
@@ -19,18 +20,19 @@ int number_of_helps;
 /* Client structure */
 typedef struct
 {
-    struct sockaddr_in address;
-    int sockfd;
-    int uid;
-    char name[32];
+    struct sockaddr_in address; //socket
+    int sockfd; //socket file descriptor
+    int uid; //socket id
+    char name[32]; //client name (random id parsed to char)
 } client_t;
 
+//clients queue
 client_t *clients[MAX_CLIENTS];
-
+//mutex to handle the insertion and removal of a client from the queue
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
-// Trim client name
+/* Trim client name */
 void str_trim_lf(char *arr, int length)
 {
     int i;
@@ -44,7 +46,10 @@ void str_trim_lf(char *arr, int length)
     }
 }
 
-/* Add clients to queue */
+/* 
+Add clients to queue. In order to add a new client to the queue
+we must verify that the client is not already in the queue.
+*/
 void queue_add(client_t *cl)
 {
     pthread_mutex_lock(&clients_mutex);
@@ -61,7 +66,7 @@ void queue_add(client_t *cl)
     pthread_mutex_unlock(&clients_mutex);
 }
 
-/* Remove clients to queue */
+/* Remove clients from the queue */
 void queue_remove(int uid)
 {
     pthread_mutex_lock(&clients_mutex);
@@ -82,7 +87,10 @@ void queue_remove(int uid)
 }
 
 
-/*This function is used to send a help message to all clients*/
+/*This function is used to send a help message to all clients. 
+We do it with mutual exclusion just for integrity purposes
+@param round : the current help message
+*/
 
 void send_help_message(int round)
 {
@@ -108,8 +116,11 @@ void send_help_message(int round)
 /* Handle all communication with the client */
 void *handle_client(void *arg)
 {
+    // buffer where we store what we want to send to the clients
     char buff_out[BUFFER_SIZE];
+    //client name
     char name[32];
+    //flag so we know when a client leaves (testing purposes)
     int leave_flag = 0;
 
     client_t *cli = (client_t *)arg;
@@ -122,7 +133,7 @@ void *handle_client(void *arg)
         {
             break;
         }
-
+        // recieve the message from the client
         int receive = recv(cli->sockfd, buff_out, BUFFER_SIZE, 0);
         if (receive > 0)
         {
@@ -137,7 +148,7 @@ void *handle_client(void *arg)
         {
             leave_flag = 1;
         }
-
+        // flush the buffer
         bzero(buff_out, BUFFER_SIZE);
     }
 
@@ -188,6 +199,7 @@ void *server_thread()
 int main(int argc, char **argv)
 {
 
+    //variables and declarations
     char *ip = "127.0.0.1";
     int port = atoi(argv[1]);
     int option = 1;
@@ -196,13 +208,12 @@ int main(int argc, char **argv)
     struct sockaddr_in cli_addr;
     pthread_t tid;
 
-    /* Socket settings */
+    /* create the socket */
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(ip);
     serv_addr.sin_port = htons(port);
 
-    /* Ignore pipe signals */
     signal(SIGPIPE, SIG_IGN);
 
     if (setsockopt(listenfd, SOL_SOCKET, (SO_REUSEPORT | SO_REUSEADDR), (char *)&option, sizeof(option)) < 0)
@@ -227,6 +238,7 @@ int main(int argc, char **argv)
 
     printf("SERVER STARTED\n");
 
+    //create a thread for the function that requests help messages
     pthread_create(&tid, NULL, &server_thread, NULL);
 
     // while true accept new connections
@@ -235,7 +247,7 @@ int main(int argc, char **argv)
         socklen_t clilen = sizeof(cli_addr);
         connfd = accept(listenfd, (struct sockaddr *)&cli_addr, &clilen);
 
-        /* Client settings */
+        /* Configure the new client */
         client_t *cli = (client_t *)malloc(sizeof(client_t));
         cli->address = cli_addr;
         cli->sockfd = connfd;
